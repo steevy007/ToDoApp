651655efc7df9f52ef3c1d2636ac355549384f5a
Steeve Sanon <steevesanon6@gmail.com>
2020-06-07 12:38:25 -0400

    Updates

---
+++ precache-manifest.2bd69826953ab8d4c20c3c6eaabbfd09.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1010a7e501f6e230f02985a832b6f87",
+    "url": "/ToDoApp/index.html"
+  },
+  {
+    "revision": "11fd4d4a6d59c9356d4a",
+    "url": "/ToDoApp/static/css/2.2836e2f2.chunk.css"
+  },
+  {
+    "revision": "79eb65d0b204ec98ad77",
+    "url": "/ToDoApp/static/css/main.8b1c3c2e.chunk.css"
+  },
+  {
+    "revision": "11fd4d4a6d59c9356d4a",
+    "url": "/ToDoApp/static/js/2.3b2b2f10.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/ToDoApp/static/js/2.3b2b2f10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79eb65d0b204ec98ad77",
+    "url": "/ToDoApp/static/js/main.e5cdabb8.chunk.js"
+  },
+  {
+    "revision": "c31936514461bb879b53",
+    "url": "/ToDoApp/static/js/runtime-main.93f28b11.js"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/ToDoApp/static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/ToDoApp/static/media/arrow_right.8ef6a08c.svg"
+  }
+]);
